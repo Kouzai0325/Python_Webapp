2fb5cdb5075d1b6f8618d97c201ace88c053e071
Kouzai0325 <2104240013ka@edu.oca.ac.jp>
2023-10-14 16:42:06 +0900

    ensyuu

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
 from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
+
+
 
 app = Flask(__name__)
 @app.route('/')
@@ -25,3 +28,24 @@ def exercise_html():
 @app.route('/exercise')
 def answer_html():
     return render_template('answer.html', name=request.args.get("my_name"))
+
+@app.route('/try_rest', methods=['POST'])
+def try_rest():
+    # リクエストデータをJSONとして受け取る
+    request_json = request.get_json()
+    print(request_json)
+    print(type(request_json))
+    name = request_json['name']
+    print(name)
+    response_json = {"response_json": request_json}
+    return jsonify(response_json)
+
+import requests
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+name = '{"name:dummy": "age:21", "friends:[dummy1,dummy2,dummy3], "is man:false": "value2"}'
+
+response = requests.post('https://127.0.0.1:5000/try_rest', headers=headers, name=name)
