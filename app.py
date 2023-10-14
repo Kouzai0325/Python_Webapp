76664a1fee53089fe78e68192f2a3729712e7653
Kouzai0325 <2104240013ka@edu.oca.ac.jp>
2023-10-14 16:04:53 +0900

    ensyuu_2

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 @app.route('/')
 def inex():
@@ -11,3 +13,15 @@ def test_request():
 @app.route('/en_request')
 def en_request():
     return f'exercise_request:{request.query_string("")}'
+
+@app.route('/show_html')
+def show_html():
+    return render_template('test_html.html')
+
+@app.route('/exercise_html')
+def exercise_html():
+    return render_template('exercise.html')
+
+@app.route('/exercise')
+def answer_html():
+    return render_template('answer.html', name=request.args.get("my_name"))
