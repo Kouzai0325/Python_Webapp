a98121ad45e6d6f47ac9ddc20860ddcb3d894bfc
Kouzai0325 <2104240013ka@edu.oca.ac.jp>
2023-10-14 15:24:05 +0900

    ensyuu

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 @app.route('/')
-def inex(): 
+def inex():
     return 'Response Data'
+
+@app.route('/test_request')
+def test_request():
+    return f'test_request:{request.args.get("dummy")}'
+
+@app.route('/en_request')
+def en_request():
+    return f'exercise_request:{request.query_string("")}'
