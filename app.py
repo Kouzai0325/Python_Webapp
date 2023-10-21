0501db2026d76ccf5c3bb0406c1e8ea475a9d461
Kouzai0325 <2104240013ka@edu.oca.ac.jp>
2023-10-21 13:20:14 +0900

    apps

--- app.py
+++ app.py
@@ -68,3 +68,46 @@ def person_result():
     search_size = request.args.get("search_size")
     persons = db.session.query(Person).filter(Person.size > search_size)
     return render_template('./person_result.html', persons=persons, search_size=search_size)
+
+@app.route('/show_html')
+def show_html():
+    return render_template('./test_html.html')
+
+
+@app.route('/try_rest', methods=['POST'])
+def try_rest():
+    # リクエストデータをJSONとして受け取る
+    request_json = request.get_json()
+    print(request_json)
+    print(type(request_json))
+    name = request_json['name']
+    print(name)
+    response_json = {"response_json": request_json}
+    return jsonify(response_json)
+
+
+@app.route('/person_search')
+def person_search():
+    return render_template('./person_search.html')
+
+
+@app.route('/person_result')
+def person_result():
+    search_size = request.args.get("search_size")
+    persons = db.session.query(Person).filter(Person.size > search_size)
+    return render_template('./person_result.html', persons=persons, search_size=search_size)
+
+
+@app.route('/try_html')
+def try_html():
+    return render_template('./try_html.html')
+
+
+@app.route('/show_data', methods=["GET", "POST"])
+def show_data():
+    return request.form
+
+
+@app.route('/try_css')
+def try_css():
+    return render_template('./try_css.html')
